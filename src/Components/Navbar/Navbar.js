376ad75a717b454da6967b234365f74658d350eb
Kaczmarczykk97 <kamil.kaczmarczykk@gmail.com>
2023-10-13 15:46:33 +0200

    navbar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,5 +1,44 @@
 import classes from "./Navbar.module.css";
 
-function Navbar() {}
+import logo from "../imgs/logo-predator.png";
+
+import { FaShoppingCart, FaSkiing, FaSnowboarding } from "react-icons/fa";
+import { GiSnowboard, GiClothes } from "react-icons/gi";
+
+function Navbar() {
+  return (
+    <div className={classes.navbar}>
+      <div className={classes["navbar-logo--wrapper"]}>
+        <img src={logo} className={classes.logo} alt="SnowyPredator's logo" />
+        <p>SnowyPredator</p>
+      </div>
+      <ul className={classes["nav-menu"]}>
+        <li className={classes["nav-menu--items"]}>
+          <FaSnowboarding className={classes.icon} />
+          Snowboard
+        </li>
+        <li className={classes["nav-menu--items"]}>
+          <FaSkiing className={classes.icon} />
+          Skiing
+        </li>
+        <li className={classes["nav-menu--items"]}>
+          <GiSnowboard className={classes.icon} />
+          Accessories
+        </li>
+        <li className={classes["nav-menu--items"]}>
+          <GiClothes className={classes.icon} />
+          Clothes
+        </li>
+      </ul>
+      <div className={classes["nav-login--btn"]}>
+        <button className={classes.btn}>
+          <p>Login</p>
+        </button>
+        <FaShoppingCart className={classes["icon--cart"]} />
+        <div className={classes["cart--counter"]}>0</div>
+      </div>
+    </div>
+  );
+}
 
 export default Navbar;
